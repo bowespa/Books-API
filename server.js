7c1b05dc2cc69147407f2c9908b9b67585fad278
Patrick Bowes <patrick@patricks-mbp.myfiosgateway.com>
2022-05-19 18:33:58 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+// DEPENDENCIES
+const express = require('express')
+const mongoose = require('mongoose')
+
+// CONFIG & MIDDLEWARE
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+app.use(express.json())
+
+// MONGOOSE
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log('connected to mongo on: ', process.env.MONGO_URI)
+})
+
+// ROOT INDEX
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+// LISTEN
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`)
+})
